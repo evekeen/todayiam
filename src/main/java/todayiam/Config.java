0d31bcc37f7050b99d344003e0d82203577b115e
Alexander Ivkin <junglefunkyman@gmail.com>
2014-09-04 19:48:48 +0400

    Changed access token

--- src/main/java/todayiam/Config.java
+++ src/main/java/todayiam/Config.java
@@ -15,8 +15,8 @@ public class Config {
     @Bean public Twitter twitter() {
         String consumerKey = "k4sT2zENvfAlqE4EY5LOz39uZ";
         String consumerSecret = "9g2hoe56ee5RQFMGm3258h4HB5ZYicBLZClgJ8hlPEyBjsyZJ4";
-        String accessToken = "2788135837-UwxZ4qTcvK8Y7anEkZfRE7Tv6mZrcGu94yD1BTc";
-        String accessTokenSecret = "bgazVxIMCsC0RyHoUbRhEgw2jGzA90Pi3KputxfL9I2YQ";
+        String accessToken = "2788135837-Y9ItYg5MvOWc7bXASEeNgGmObvL1TYqcYFFqFDX";
+        String accessTokenSecret = "uxMj6EiN7S4bltzo9uSqvg2lK3MJ9MZecva8VsLdgEWUE";
         return new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
     }
 
